921da756b2cb6435de213582f71cff7f0f4fc97c
nitesh <niteshkrjha.nkj@gmail.com>
2023-09-12 01:32:31 +0530

    made responsive

--- src/components/Login.js
+++ src/components/Login.js
@@ -77,7 +77,6 @@ const Login = () => {
       )
         .then((userCredential) => {
           const user = userCredential.user;
-          // console.log(user);
         })
         .catch((error) => {
           const errorCode = error.code;
